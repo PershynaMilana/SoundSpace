eba6ccb4caee7dbb057af8811dee2e9fdeacc622
Milana Pershina <mimilik07@gmail.com>
2023-11-08 10:52:06 +0200

    Twenty-fifth commit

--- frontend/src/components/CreatePlaylistModal.js
+++ frontend/src/components/CreatePlaylistModal.js
@@ -1,158 +1,217 @@
 import React, { useState } from "react";
 import {
-  Modal,
-  Paper,
-  Typography,
-  Button,
-  Grid,
-  IconButton,
+    Modal,
+    Paper,
+    Typography,
+    Button,
+    Grid,
+    IconButton,
 } from "@mui/material";
 import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
-import MusicNoteIcon from "@mui/icons-material/MusicNote";
-import {
-  getStorage,
-  ref,
-  uploadBytes,
-  getDownloadURL,
-} from "firebase/storage";
+
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { app } from "../services/fairbaseConfig";
 import "../assets/styles/modalWindow.css";
+import DefaultPhoto from "../assets/images/default-image.jpg";
 
 const CreatePlaylistModal = ({ open, onClose, onCreatePlaylist }) => {
-  const [playlistName, setPlaylistName] = useState("");
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [imageUrl, setImageUrl] = useState("");
-  const [playlistDescription, setPlaylistDescription] = useState("");
-  const [isHovered, setIsHovered] = useState(false);
+    const [playlistName, setPlaylistName] = useState("");
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [imageUrl, setImageUrl] = useState("");
+    const [playlistDescription, setPlaylistDescription] = useState("");
+    const [isHovered, setIsHovered] = useState(false);
 
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    setSelectedImage(file);
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        setSelectedImage(file);
 
-    const storage = getStorage(app);
-    const storageRef = ref(storage, `images/${file.name}`);
-    uploadBytes(storageRef, file).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((url) => {
-        setImageUrl(url);
-      });
-    });
-  };
+        const storage = getStorage(app);
+        const storageRef = ref(storage, `images/${file.name}`);
+        uploadBytes(storageRef, file).then((snapshot) => {
+            getDownloadURL(snapshot.ref).then((url) => {
+                setImageUrl(url);
+            });
+        });
+    };
 
-  const handleCreatePlaylist = () => {
-    if (!playlistName || !imageUrl) {
-      return;
-    }
+    const handleCreatePlaylist = () => {
+        if (!playlistName || !imageUrl) {
+            return;
+        }
 
-    onCreatePlaylist(playlistName, imageUrl, playlistDescription);
+        onCreatePlaylist(playlistName, imageUrl, playlistDescription);
 
-    setPlaylistName("");
-    setSelectedImage(null);
-    setImageUrl("");
-    setPlaylistDescription("");
-  };
+        setPlaylistName("");
+        setSelectedImage(null);
+        setImageUrl("");
+        setPlaylistDescription("");
+    };
 
-  const photoSelectButtonStyle = {
-    width: "270px",
-    height: "280px",
-    margin: "10px",
-    background: isHovered
-      ? "linear-gradient(to bottom, #000000 0%, #222121 100%)"
-      : "linear-gradient(to bottom, #000000 0%, #444444 100%)",
-    borderRadius: "5px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  };
+    const photoSelectButtonStyle = {
+        width: "270px",
+        height: "280px",
+        margin: "10px",
+        background: isHovered
+            ? "linear-gradient(to bottom, #222222 0%, #222222 100%)"
+            : "linear-gradient(to bottom, #222222 0%, #222222 100%)",
+        borderRadius: "5px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "relative",
+    };
 
-  return (
-    <Modal open={open} onClose={onClose}>
-      <Paper
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          padding: "20px",
-          width: "600px",
-          height: "350px",
-          background: "linear-gradient(to bottom, #000000 0%, #222121 60%)",
-        }}
-      >
-        <Grid container spacing={2} style={{ height: "100%" }}>
-          <Grid item xs={6}>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
+    const addPhotoIconStyle = {
+        color: "white",
+    };
+
+    const backgroundImageStyle = {
+        position: "absolute",
+        zIndex: 0,
+        width: "100%",
+        height: "100%",
+        objectFit: "cover",
+        opacity: isHovered ? 0.5 : 1,
+        borderRadius: "5px",
+    };
+
+    return (
+        <Modal open={open} onClose={onClose}>
+            <Paper
+                style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    padding: "20px",
+                    width: "600px",
+                    height: "300px",
+                    background: "#333333",
+                }}
             >
-              <input
-                type="file"
-                accept="image/*"
-                onChange={handleImageChange}
-                style={{ display: "none" }}
-                id="image-input"
-              />
-              <label
-                htmlFor="image-input"
-                style={{ cursor: "pointer" }}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              >
-                <IconButton
-                  color="primary"
-                  component="span"
-                  style={photoSelectButtonStyle}
-                >
-                  {isHovered ? (
-                    <>
-                      <AddAPhotoIcon />
-                      <Typography variant="subtitle1">
-                        Выбрать фото
-                      </Typography>
-                    </>
-                  ) : (
-                    <img
-                      src={imageUrl} 
-                      alt="Selected Image"
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                        display: imageUrl ? "block" : "none", 
+                <Grid container spacing={2} style={{ height: "100%" }}>
+                    <Grid item xs={6}>
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                            }}
+                        >
+                            <input
+                                type="file"
+                                accept="image/*"
+                                onChange={handleImageChange}
+                                style={{ display: "none" }}
+                                id="image-input"
+                            />
+                            <label
+                                htmlFor="image-input"
+                                style={{ cursor: "pointer" }}
+                                onMouseEnter={() => setIsHovered(true)}
+                                onMouseLeave={() => setIsHovered(false)}
+                            >
+                                <IconButton
+                                    color="primary"
+                                    component="span"
+                                    style={photoSelectButtonStyle}
+                                >
+                                    {imageUrl && (
+                                        <img
+                                            src={imageUrl}
+                                            alt="Selected Image"
+                                            style={{
+                                                width: "100%",
+                                                height: "100%",
+                                                objectFit: "cover",
+                                                position: "absolute",
+                                                zIndex: 1,
+                                                borderRadius: "5px",
+                                            }}
+                                        />
+                                    )}
+                                    <img
+                                        src={DefaultPhoto}
+                                        alt="Default Image"
+                                        style={{ ...backgroundImageStyle }}
+                                    />
+                                    {isHovered && !imageUrl && (
+                                        <div
+                                            style={{
+                                                position: "absolute",
+                                                zIndex: 2,
+                                            }}
+                                        >
+                                            <AddAPhotoIcon
+                                                style={addPhotoIconStyle}
+                                            />
+                                            <Typography
+                                                style={{ color: "white" }}
+                                                variant="subtitle1"
+                                            >
+                                                Выбрать фото
+                                            </Typography>
+                                        </div>
+                                    )}
+                                </IconButton>
+                            </label>
+                        </div>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <br />
+                        <Typography
+                            color={"white"}
+                            variant="h5"
+                            style={{ fontWeight: "500" }}
+                        >
+                            Создать плейлист
+                        </Typography>
+                        <br />
+                        <Typography color={"white"} variant="p">
+                            Название плейлиста:
+                        </Typography>
+                        <input
+                            style={{ marginTop: "10px", marginBottom: "10px" }}
+                            label="Название плейлиста"
+                            className="input-fields-modal"
+                            value={playlistName}
+                            onChange={(e) => setPlaylistName(e.target.value)}
+                        />
+                        <br />
+                        <Typography color={"white"} variant="p">
+                            Описание плейлиста:
+                        </Typography>
+                        <input
+                            style={{ marginTop: "10px", marginBottom: "10px" }}
+                            label="Описание плейлиста"
+                            className="input-fields-modal"
+                            value={playlistDescription}
+                            onChange={(e) =>
+                                setPlaylistDescription(e.target.value)
+                            }
+                            fullWidth
+                        />
+                        <Button
+                            className="button-18"
+                            onClick={handleCreatePlaylist}
+                            style={{
+                              justifyContent: "center",
+                              aligniItems: "center",
+                            margin:"10px",
+                          marginLeft: "20px",
+                        borderRadius:"30px", 
+                        backgroundColor:"#cccccc",
+                      color:"black",
+                      fontWeight:"600"
                       }}
-                    />
-                  )}
-                </IconButton>
-              </label>
-            </div>
-          </Grid>
-          <Grid item xs={6}>
-            <Typography color={"white"} variant="h6">
-               Создать плейлист
-            </Typography>
-            <input
-              label="Название плейлиста"
-              className="input-fields-modal"
-              value={playlistName}
-              onChange={(e) => setPlaylistName(e.target.value)}
-            />
-            <input
-              label="Описание плейлиста"
-              className="input-fields-modal"
-              value={playlistDescription}
-              onChange={(e) => setPlaylistDescription(e.target.value)}
-              fullWidth
-            />
-            <Button className="button-18" onClick={handleCreatePlaylist}>
-              Создать плейлист
-            </Button>
-          </Grid>
-        </Grid>
-      </Paper>
-    </Modal>
-  );
+                        >
+                            Создать плейлист
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Paper>
+        </Modal>
+    );
 };
 
 export default CreatePlaylistModal;
